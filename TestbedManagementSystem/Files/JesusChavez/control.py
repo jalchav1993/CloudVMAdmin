0ce882bba1464f7ad265cb48ed77901856fc1f5e
Aex <aex@Aexs-MacBook-Pro.local>
2017-11-29 16:58:55 -0700

    control

--- TestbedManagementSystem/Files/JesusChavez/control.py
+++ TestbedManagementSystem/Files/JesusChavez/control.py
@@ -1,1 +1,73 @@
-#should route traffic from gui
+import os
+import requests
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/workshop/')
+def get():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/workshop/create/')
+def create():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/workshop/update/')
+def update():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/workshop/delete/')
+def delete():
+    errors = []
+    results = {}
+    return "test"
+    
+@app.route('/user/')
+def get():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/user/create/')
+def create():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/user/update/')
+def update():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/user/delete/')
+def delete():
+    errors = []
+    results = {}
+    return "test"
+    
+@app.route('/hardware/poll')
+def poll():
+    return "test";
+@app.route('/hardware/clone_vm/')
+def clone():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/hardware/create_vm/')
+def create():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/hardware/update_vm/')
+def create():
+    errors = []
+    results = {}
+    return "test"
+@app.route('/hardware/delete_vm/')
+def create():
+    errors = []
+    results = {}
+    return "test"
+if __name__ == '__main__':
+    app.run()
