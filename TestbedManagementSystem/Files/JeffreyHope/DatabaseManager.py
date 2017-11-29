422420e0cd3b8a3f81bd9425b36604096e2165e3
StrangerCoug <strangercoug@hotmail.com>
2017-11-29 09:41:52 -0700

    Modify DatabaseManager.py

--- TestbedManagementSystem/Files/JeffreyHope/DatabaseManager.py
+++ TestbedManagementSystem/Files/JeffreyHope/DatabaseManager.py
@@ -1,3 +1,11 @@
+from AdminDB import AdminDB
+from RegUserDB import RegUserDB
+from ServerDB import ServerDB
+from VirtualMachineDB import VirtualMachineDB
+from WorkshopDB import WorkshopDB
+from WorkshopGroupDB import WorkshopGroupDB
+from WorkshopUnitDB import WorkshopUnitDB
+
 class DatabaseManager:
     def __init__(self):
         print("Database Manager Created")
