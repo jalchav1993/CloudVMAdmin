20b6ee2e1fd0da97037b012ff0bcfb02db528c6f
Aex <aex@Aexs-MacBook-Pro.local>
2017-11-29 17:16:58 -0700

    control

--- TestbedManagementSystem/Files/JesusChavez/control.py
+++ TestbedManagementSystem/Files/JesusChavez/control.py
@@ -4,23 +4,24 @@ from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
-@app.route('/workshop/')
+@app.route('/workshop/', methods=['GET', 'POST'])
 def get():
     errors = []
     results = {}
     return "test"
-@app.route('/workshop/create/')
-def create():
+@app.route('/workshop/create/<wsid>')
+#this uses get
+def create(wsid):
     errors = []
     results = {}
     return "test"
-@app.route('/workshop/update/')
-def update():
+@app.route('/workshop/update/<wsid>')
+def update(wsid):
     errors = []
     results = {}
     return "test"
-@app.route('/workshop/delete/')
-def delete():
+@app.route('/workshop/delete/<>')
+def delete(wsid):
     errors = []
     results = {}
     return "test"
@@ -30,42 +31,55 @@ def get():
     errors = []
     results = {}
     return "test"
-@app.route('/user/create/')
-def create():
+@app.route('/user/login/')
+#this uses post
+def login():
     errors = []
     results = {}
     return "test"
-@app.route('/user/update/')
-def update():
+@app.route('/user/logour/')
+def login():
     errors = []
     results = {}
     return "test"
-@app.route('/user/delete/')
-def delete():
+@app.route('/user/create/<userid>')
+def create(userid):
     errors = []
     results = {}
     return "test"
-    
+@app.route('/user/update/<userid>')
+def update(userid):
+    errors = []
+    results = {}
+    return "test"
+@app.route('/user/delete/<userid>')
+def delete(userid):
+    errors = []
+    results = {}
+    return "test"
+@app.route('/hardware/')
+def get():
+    return "test";
 @app.route('/hardware/poll')
 def poll():
     return "test";
-@app.route('/hardware/clone_vm/')
-def clone():
+@app.route('/hardware/clone_vm/<vm_id>')
+def clone(vm_id):
     errors = []
     results = {}
     return "test"
-@app.route('/hardware/create_vm/')
-def create():
+@app.route('/hardware/create_vm/<vm_id>')
+def create(vm_id):
     errors = []
     results = {}
     return "test"
-@app.route('/hardware/update_vm/')
-def create():
+@app.route('/hardware/update_vm/<vm_id>')
+def create(vm_id):
     errors = []
     results = {}
     return "test"
-@app.route('/hardware/delete_vm/')
-def create():
+@app.route('/hardware/delete_vm/<vm_id>')
+def create(vm_id):
     errors = []
     results = {}
     return "test"
