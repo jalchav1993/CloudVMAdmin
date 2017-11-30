df416eba309d2575ffb872d8fed64bbb6a10bf08
Aex <aex@Aexs-MacBook-Pro.local>
2017-11-29 19:56:37 -0700

    routes

--- TestbedManagementSystem/Files/JesusChavez/stargate/hardware/routes.py
+++ TestbedManagementSystem/Files/JesusChavez/stargate/hardware/routes.py
@@ -3,3 +3,27 @@ from . import hardware
 @hardware.route("/hardware/get")
 def get():
     return HardwareManager.getVM();
+
+@hardware.route("/hardware/get/VRDP/<name>")
+def get(name):
+    return HardwareManager.getVRDP(name);
+    
+@hardware.route("/hardware/get/networ_adapter/<name>")
+def get (name):
+    return HardwareManager.getNetworkAdapter(name) 
+    
+@hardware.route("/hardware/get/host_server/<name>") 
+def get(name):
+    return HardwareManager.getHostServer(name)
+    
+@hardware.route("/hardware/get/cpu_use/<name>") 
+def get(name):
+    return HardwareManager.getCPUusage(name)
+    
+@hardware.route("/hardware/get/get_memory_use/<name>") 
+def get(name):
+    return HardwareManager.getMemoryusage(name)
+
+@hardware.route("/hardware/get/create_clone/<vmName, numOfClones>") 
+def get(ip):
+    return HardwareManager.createClone(vmName, numOfClones)(name)
--- TestbedManagementSystem/Files/JesusChavez/stargate/workshop/routes.py
+++ TestbedManagementSystem/Files/JesusChavez/stargate/workshop/routes.py
@@ -1,4 +1,15 @@
 from . import workshop
-@workshop.route("/workshop/get")
-def hello():
-    return "Hello World!"
+@users.route("/workshop/<ws>")
+def getWorkshops(ws):
+    return WorkshopManager.getWorkshop(ws);
+@users.route("/workshop/create/<ws>")
+def create(ws):
+    return WorkshopManager.createWorkshop(ws);
+@users.route("/workshop/update/<ws>")
+def update(ws):
+    return WorkshopManager.updateWorkshop(ws);
+@users.route("/workshop/delete/<ws>")
+def delete(ws):
+    return WorkshopManager.deleteWorkshop(ws);
+
+from . import users
