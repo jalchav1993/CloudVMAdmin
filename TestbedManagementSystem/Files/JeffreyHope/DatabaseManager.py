1c9c3d8c693a820d265b81478c719bf861793a88
StrangerCoug <strangercoug@hotmail.com>
2017-11-29 20:01:47 -0700

    Modify DatabaseManager.py

--- TestbedManagementSystem/Files/JeffreyHope/DatabaseManager.py
+++ TestbedManagementSystem/Files/JeffreyHope/DatabaseManager.py
@@ -9,3 +9,24 @@ from WorkshopUnitDB import WorkshopUnitDB
 class DatabaseManager:
     def __init__(self):
         print("Database Manager Created")
+
+    def __getAdminDB(self)
+        return "something"
+
+    def __getRegUserDB(self)
+        return "something"
+
+    def __getServerDB(self)
+        return "something"
+
+    def __getVirtualMachineDB(self)
+        return "something"
+
+    def __getWorkshopDB(self)
+        return "something"
+
+    def __getWorkshopGroupDB(self)
+        return "something"
+
+    def __getWorkshopUnitDB(self)
+        return "something"
