98e0106a5b27bc5a8a857727095543f37f9e0f30
LCCuellar <lccuellar@miners.utep.edu>
2017-12-15 23:31:36 -0700

    ahem... AAAAAAAAHHHHHHHHHHH

--- TestbedManagementSystem/Files/LuisCuellar/TestIntegration.py
+++ TestbedManagementSystem/Files/LuisCuellar/TestIntegration.py
@@ -1,39 +1,54 @@
 from WorkshopManager import WorkshopManager
 from HardwareManager import HardwareManager
 from time import sleep
+import sys
 
 vm1 = {"vmname": "vmTest", "vrdp": {'ip': "172.19.156.75", 'port': 8000}, "networkAdapter": "testAdapter", "host":"172.19.156.75"}
 vm2 = {"vmname": "vmUnitTest", "vrdp": {'ip': "172.19.156.75", 'port': 4000}, "networkAdapter": "testAdapter", "host":"172.19.156.75"}
 
 vms = {vm1['vmname'], vm2['vmname']}
 
-unit1 = {"unitName": "unit1", "vms": vms, "description": "fuck", "referenceMaterial": "fuck.txt",
-         "connectionString": "something", "sessionType": "persistent", "status": "available"}
+unit1 = {"unitName": "unit1", "vms": vms, "description": "desc", "referenceMaterial": "filename.txt",
+         "connectionString": "172.19.156.75", "sessionType": "persistent", "status": "available"}
+
+
 
 hwmgr = HardwareManager()
 wsmgr = WorkshopManager()
 
-hwmgr.addVirtualMachine(vm1)
-hwmgr.addVirtualMachine(vm2)
+line = ""
 
-wsmgr.addWorkshopUnit(unit1)
-
-#wsmgr.startUnit( "unit1")
-#sleep(5)
-#wsmgr.pauseUnit("unit1")
-#sleep(5)
-#wsmgr.resumeUnit("unit1")
-#sleep(5)
-#wsmgr.power_downUnit("unit1")
-
-newUnits = wsmgr.cloneUnit("unit1", 2)
-print( len(newUnits) )
-
-ws1 = {"groupName": "group1", "associatedUnits": newUnits, "description": "fuck", "referenceMaterial": "fuck.txt",
-       "sessionType": "persistent", "endDate": "12/15/2017", "status": "okayFam"}
-
-wsmgr.addWorkshopGroup(ws1)
-
-sleep(5)
-
-wsmgr.startGroup("group1")
+while( line != "EXIT"):
+    line = input()
+    if(line == "Add VMs"):
+        hwmgr.addVirtualMachine(vm1)
+        hwmgr.addVirtualMachine(vm2)
+        
+    elif(line == "Add Unit"):
+        wsmgr.addWorkshopUnit(unit1)
+    elif(line == "Start Unit"):
+        wsmgr.startUnit( "unit1")
+    elif(line == "Pause Unit"):
+        wsmgr.pauseUnit("unit1")
+    elif(line == "Resume Unit"):
+        wsmgr.resumeUnit("unit1")
+    elif(line == "Power Down Unit"):
+        wsmgr.power_downUnit("unit1")
+        
+        
+    elif(line == "Add Group"):
+        newUnits = wsmgr.cloneUnit("unit1", 2)
+        ws1 = {"groupName": "group1", "associatedUnits": newUnits, "description": "desc", "referenceMaterial": "filename.txt",
+               "sessionType": "persistent", "endDate": "12/15/2017", "status": "available"}        
+        wsmgr.addWorkshopGroup(ws1)
+        
+    elif(line == "Start Group"):
+        wsmgr.startGroup( "group1")
+    elif(line == "Pause Group"):
+        wsmgr.pauseGroup("group1")
+    elif(line == "Resume Group"):
+        wsmgr.resumeGroup("group1")
+    elif(line == "Power Down Unit"):
+        wsmgr.power_downGroup("group1")
+    else:
+        break
