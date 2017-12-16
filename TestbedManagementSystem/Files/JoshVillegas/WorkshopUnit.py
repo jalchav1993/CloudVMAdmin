e4ae68ad27f61ba57646835ac3f36344d629ef44
LCCuellar <lccuellar@miners.utep.edu>
2017-12-15 18:03:05 -0700

    Changes to Workshop Subsystem

--- TestbedManagementSystem/Files/JoshVillegas/WorkshopUnit.py
+++ TestbedManagementSystem/Files/JoshVillegas/WorkshopUnit.py
@@ -8,7 +8,8 @@ class WorkshopUnit:
         self.__connectionString = configuration['connectionString']
         self.__sessionType = configurations['sessionType']
         self.__status = configurations['status']
-        self.__createUnitRecord()
+        self.__hwmgr = HardwareManager.instance()
+        #self.__createUnitRecord()
         
     def getVmList(self):
         return self.__vms
@@ -18,6 +19,12 @@ class WorkshopUnit:
     
     def getConnectionString(self):
         return self.__connectionString
+    
+    def getStatus(self):
+        return self.__status
+    
+    def isAvailable(self):
+        return self.__status == 0 #Available
     
     def __createUnitRecord(self):
         dbMgr = DatabaseManager.Instance()
@@ -38,48 +45,65 @@ class WorkshopUnit:
             self.startVM(vmname)
             
     def startVM(self, vmname):
-        hwMgr = HardwareManager.instance()
         if vmname in vms:
-            hwMgr.startVM(vmname)
+            self.__hwMgr.startVM(vmname)
             
     # the followig methods exist but still need to be implemented
     
-    def pauseVm(self, vmname):
+    def pauseVM(self, vmname):
+        if vmname in self.__vms:
+            self.__hwmgr.pauseVM(vmname)
     
-    def pauseAllVms(self):
-        
-    def suspendVm(self, vmname):
+    def pauseAllVMs(self):
+        for vmname in self.__vms:
+            self.pauseVM(vmname)
+            
+    def resumeVM(self, vmname):
+        if vmname in self.__vms:
+            self.__hwmgr.resumeVM(vmname)
     
-    def suspendAllVms(self):
+    def resumeAllVMs(self):
+        for vmname in self.__vms:
+            self.resumeVM(vmname)            
     
-    def removeVm(self, vmname):
+    def power_downVM(self, vmname):
+        if vmname in self.__vms:
+            self.__hwmgr.power_downVM(vmname)
+    def power_downAllVMs(self):
+        for vmname in self.__vms:
+            self.power_downVM(vmname)
+    
+    def removeVM(self, vmname):
         if vmname in vms:
             vms.remove(vmname)
         
-    def removeAllVms(self):
+    def removeAllVMs(self):
         vms.clear()
         
-    def createUnit(self, configurations):
-        newUnit = WorkshopUnit(configurations)
-        new = set()
-        for vm in vms:
-            set.add(HardwareManager.clone(vm))
+    #def createUnit(self, configurations):
+        #newUnit = WorkshopUnit(configurations)
+        #new = set()
+        #for vm in vms:
+            #set.add(HardwareManager.clone(vm))
             
     def cloneUnits(self, numUnits):
-       newUnits = set()
-       newVms = set()
-       for i in range(0, numUnits):
-           uName = self.__unitName + i
-           uPort = self.__connectionString += 1
-           for each vm in self.__vms:
-               newVm = HardwareManager.clone(vm)
-               newVms.add(newVm)
-            newconfig = {"unitName" : uName, "vms" : newVms, "description" : self.__description, "referenceMaterial" : self.__referenceMaterial, "connectionString" : uPort, "sessionType" : self.__sessionType, "status" : self.__status} 
-            #need to configure a new dictionary 
-            newUnit = WorkshopUnit(newConfig)
-            newUnits.add(newUnit)
-        return newUnits
+        newUnits = set()
         
+        for i in range(0, numUnits):
+            uName = self.__unitName + i
+            uPort = self.__connectionString
+            newVms = set()
+            
+            for vm in self.__vms:
+                newVm = HardwareManager.clone(vm)
+                newVms.add(newVm)
+            
+            unitConfig = {"unitName" : uName, "vms" : newVms, "description" : self.__description, "referenceMaterial" : self.__referenceMaterial, "connectionString" : uPort, "sessionType" : self.__sessionType, "status" : self.__status} 
+            newUnit = WorkshopUnit(unitConfig)
+            newUnits.add(unitConfig)
+            
+        return newUnits
+     
     def unitDetailView(self):
         return configurations
                 
